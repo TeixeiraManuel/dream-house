de5f56282b73c9794cf4392c931e7ce27bd2d089
TeixeiraManuel <teixeiramanuelteidy@gmail.com>
2024-11-26 04:54:28 -0300

    feat: created hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,57 @@
+import { ShoppingBag } from "lucide-react";
+
 import { Header } from "./components/header";
-//import heroImage from "../src/assets/heroImg.png";
+
+import heroImage from "../src/assets/heroImg.png";
+import otherImgHero from "../src/assets/othersImgHero.png";
+import elipseImg from "../src/assets/Ellipse.png";
+import smallElipseImg from "../src/assets/smallEllipse.png";
+
 export function App() {
   return (
     <div>
       <div className="mx-14 h-screen relative pt-14 space-y-36">
         <Header />
+        <div className="flex items-center gap-6">
+          <img
+            className="h-[45rem] w-[50rem] before:content-[''] before:block before:absolute before:w-full before:h-[100vh] before:bg-no-repeat before:-left-26 before:top-[10px] before:z-50  before:bg-[url('/src/assets/circleBeforeHero.png')]"
+            src={heroImage}
+            alt="Hero image"
+          />
+          <div className="space-y-5 relative">
+            <h1 className="font-bold text-3xl w-[60rem]">
+              <span className="text-secondary">Casa dos Sonhos</span>, onde
+              ajudamos você a encontrar o lar perfeito para sua família.
+              descobrir uma seleção exclusiva de imóveis que atendem a todas as
+              suas necessidades.
+            </h1>
+            <p className="text-lg">
+              Com anos de experiência no mercado imobiliário, estamos prontos
+              para guiá-lo <br /> em cada etapa do processo.
+            </p>
+            <button className="text-slate-100 bg-secondary w-40 h-10 font-bold flex items-center justify-center gap-2 rounded-md">
+              Comprar agora
+              <ShoppingBag size={18} />
+            </button>
+            <img src={elipseImg} alt="" className="absolute right-0 top-32" />
+            <div className="flex items-start gap-32">
+              <img src={otherImgHero} alt="" />
+              <div>
+                <h5 className="font-bold text-xl">123 Milhões+</h5>
+                <p className="font-bold">
+                  Descubra nossa ampla variedade de imóveis, desde <br />
+                  aconchegantes apartamentos até espaçosas <br /> casas em
+                  bairros prestigiados
+                </p>
+              </div>
+            </div>
+            <img
+              src={smallElipseImg}
+              alt=""
+              className="absolute right-36 top-42"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import logo from "/logo.svg";
 export function Header() {
   return (
-    <div className="absolute w-full">
+    <header className="absolute w-full">
       <nav
         className="flex  justify-between px-14 bg-primary h-24 rounded-lg relative header after:content-[''] after:block after:absolute after:w-full after:h-[400px] after:bg-no-repeat after:bg-right after:bottom-[-350px]  after:z-[-1] after:bg-[url('/src/assets/lampHeader.png')]
                   before:content-[''] before:block before:absolute before:w-full before:h-[100vh] before:bg-no-repeat before:bg-left before:top-[10px] before:z-[-1]  before:bg-[url('/src/assets/lineHeader.png')]"
@@ -38,11 +38,11 @@ export function Header() {
         </ul>
         <div className="flex items-center gap-8">
           <button>Login</button>
-          <button className="bg-white text-primary font-bold w-36 h-9 rounded-sm">
+          <button className="bg-white text-primary font-bold w-36 h-9 rounded-md">
             Cadastro
           </button>
         </div>
       </nav>
-    </div>
+    </header>
   );
 }
